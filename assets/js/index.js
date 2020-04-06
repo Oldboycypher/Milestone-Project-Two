3f153a198fd1b191f8c8a10928f582270395c4be
Paul Kitching <paul.kitching@hotmail.co.uk>
2020-04-06 18:45:38 +0000

    basic site structure with responsive top nav

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+
+
+//make topnav responsive
+
+function icon() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
